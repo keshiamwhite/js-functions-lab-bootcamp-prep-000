3229893ae4db7432153257765a1747b2b944d6fa
Keshia White <keshiamwhite@gmail.com>
2018-08-13 23:44:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // write your code below!
+function happyHolidays() {
+  console.log('Happy holidays!')
+}
+
+function happyHolidayTo(holiday, name) {
+  return (`Happy ${holiday}, ${name}!`)
+}
+
+function holidayCountdown(days, holiday) {
+  return (`It's ${days} days until ${holiday}!`)
+}
